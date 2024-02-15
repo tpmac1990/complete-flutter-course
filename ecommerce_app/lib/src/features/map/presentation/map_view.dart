9dfe62b457aa2e6bfb8bc9b549dbb6fa28b9c89a
tpmac1990 <terence.paulmac@gmail.com>
2024-02-15 18:03:46 +1100

    add button to move map camera on click

--- ecommerce_app/lib/src/features/map/presentation/map_view.dart
+++ ecommerce_app/lib/src/features/map/presentation/map_view.dart
@@ -1,10 +1,36 @@
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
 
 
-class MapViewScreen extends StatelessWidget {
-  const MapViewScreen({super.key});
+class MapViewScreen extends StatefulWidget {
+  const MapViewScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MapViewScreen> createState() => _MapViewScreenState();
+}
+
+class _MapViewScreenState extends State<MapViewScreen> {
+  late final Completer<GoogleMapController> _controller; // Remove const here
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = Completer<GoogleMapController>(); // Initialize _controller here
+  }
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static const CameraPosition _kLake = CameraPosition(
+    bearing: 192.8334901395799,
+    target: LatLng(37.43296265331129, -122.08832357078792),
+    tilt: 59.440717697143555,
+    zoom: 19.151926040649414,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -12,19 +38,23 @@ class MapViewScreen extends StatelessWidget {
       appBar: AppBar(title: Text('Map'.hardcoded)),
       body: GoogleMap(
         initialCameraPosition: const CameraPosition(
-          target: LatLng(37.7749, -122.4194), // Initial map coordinates (San Francisco, CA)
+          target: LatLng(37.7749, -122.4194),
           zoom: 12.0,
         ),
         onMapCreated: (GoogleMapController controller) {
-          // You can use the controller to interact with the map
-          // For example, you can animate to a specific location:
-          controller.animateCamera(
-            CameraUpdate.newLatLng(
-              const LatLng(37.7749, -122.4194)
-            )
-          );
+          _controller.complete(controller);
         },
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: const Text('To the lake!'),
+        icon: const Icon(Icons.directions_boat),
+      ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
